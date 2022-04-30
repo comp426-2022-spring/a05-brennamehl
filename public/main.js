23ed3b9923ad994fb10460a0ac3955b10d6cec11
brennamehl <brennam@email.unc.edu>
2022-04-30 12:34:52 -0400

    added code from class example

--- public/main.js
+++ public/main.js
@@ -29,7 +29,29 @@ function clickGuess(){
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
+const coins = document.getElementById("coins")
+// Add event listener for coins form
+coins.addEventListener("submit", flipCoins)
+// Create the submit handler
+async function flipCoins(event) {
+    event.preventDefault();
+    
+    const endpoint = "app/flip/coins/"
+    const url = document.baseURI+endpoint
 
+    const formEvent = event.currentTarget
+
+    try {
+        const formData = new FormData(formEvent);
+        const flips = await sendFlips({ url, formData });
+
+        console.log(flips);
+        document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+        document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+    } catch (error) {
+        console.log(error);
+    }
+}
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
