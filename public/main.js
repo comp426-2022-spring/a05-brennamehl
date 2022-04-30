24c915450f4dc7d97eb896e46ec1036f75637ecf
brennamehl <brennam@email.unc.edu>
2022-04-30 12:20:30 -0400

    added clickhome function

--- public/main.js
+++ public/main.js
@@ -1,8 +1,16 @@
 // Focus div based on nav button click
+function clickHome(){
+    document.getElementById("homenav").className = "active";
+    document.getElementById("singlenav").className = "hidden";
+    document.getElementById("multinav").className = "hidden";
+    document.getElementById("guessnav").className = "hidden";
+    
 
+}
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
+
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
